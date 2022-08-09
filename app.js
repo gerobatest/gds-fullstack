ca143fb3bfd8d9b0d8978e81edbc83338f601aa3
gerobatest <108824541+gerobatest@users.noreply.github.com>
2022-08-09 15:17:16 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,40 +1,59 @@
 const express = require('express')
-const mongoose = require("mongoose");//Add mongoose : database 
-
-
 app = express()
+
+const cors = require('cors') //cross-origin
 
 require('dotenv').config();
 
+//const mongoose = require("mongoose");//Add mongoose : database 
+
 //Middlewares
+app.use(cors());
 app.use(express.json());
+app.use(require("./routes/record"))
 app.use(express.urlencoded({ extended: true }));
 
+//get driver connections 
+const dbo = require("./db/conn")
+
+//configuration du port 
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => {
+
+    //connect avec la base donnée quand le serveur démarre
+    dbo.connectToServer(function (err) {
+      if(err) console.error(err);
+    })
+    console.log(`Listening on Port: ${PORT}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+//app.set('view engine', 'ejs')
+
+//app.use('/', require('./routes/hello'))
+//app.use('/api/', require('./routes/hello'))
 
 //Connexion à la base de donnée 
-const connectDb = () => {
+/*const connectDb = () => {
   try{
     mongoose.connect(process.env.DB_URI);
     console.log("Database connected");
   }catch (error){
     console.log(error);
   }
-}
-
-
-app.set('view engine', 'ejs')
-
-
-//app.use('/', require('./routes/hello'))
-app.use('/api/', require('./routes/hello'))
+}*/
 
 
 // app.get('/', (req, res) => {
 //     res.send('hello haingo. How are you?')
 // })
-
-const PORT = process.env.PORT || 4000
-
-app.listen(PORT, () => {
-    console.log(`Listening on Port: ${PORT}`)
-})
